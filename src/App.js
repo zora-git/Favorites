81c4a2bf72c97c1d2647268ab2f29c7b00089af0
zora-git <154859133+zora-git@users.noreply.github.com>
2024-03-08 14:55:41 +0900

    made homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="Home">
+      <div className="Intro">
+        <a href="/art-tech.html">
+          <h1 className="Hero">
+            H<span className="Name">yun</span> J
+            <span className="Name">ung</span> Park
+          </h1>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
